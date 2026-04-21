8fc752abdd1fc7bc4571e5fa5d08d68d1b4e50c4
elisyanez <elis.yanez@duocuc.cl>
2026-04-21 14:45:15 -0400

    Corregido Modulos con TypeScript para compilado exitoso

--- Producto/LexiScan_Angular/lexi-scan/src/app/tab2/tab2.page.ts
+++ Producto/LexiScan_Angular/lexi-scan/src/app/tab2/tab2.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
 import { IRegistro } from '../models/auth.model';
 
